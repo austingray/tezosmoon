609de657b57e90753e6a3d0c63fb6af034ff9970
Austin Gray <waustingray@gmail.com>
2021-09-07 19:56:05 -0600

    added twitter card to site meta

--- components/TMHead.tsx
+++ components/TMHead.tsx
@@ -6,6 +6,18 @@ function TMHead({ title }) {
       <title>{title}</title>
       <meta name="description" content="Tezos Moon App" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@tezosmoonapp" />
+      <meta name="twitter:title" content="Tezos Moon - NFT Browser" />
+      <meta
+        name="twitter:description"
+        content="Browse and manage Tezos NFTs."
+      />
+      <meta
+        name="twitter:image"
+        content="https://tezosmoonapp.com/img/tezos-moon-306x306.png"
+      />
     </Head>
   );
 }
