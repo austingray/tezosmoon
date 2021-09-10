74fb378722c2d84c9f4cee97ce1f8184620be8dd
Austin Gray <waustingray@gmail.com>
2021-09-10 00:40:21 -0600

    updated homepage language and links

--- components/Login.tsx
+++ components/Login.tsx
@@ -27,6 +27,8 @@ class Login extends React.Component<any, any> {
           <div>
             <h2 className="text-4xl my-4">Welcome to Tezos Moon [alpha]</h2>
             <p>
+              Hi, welcome to my{" "}
+              <a href="https://www.hicetnunc.xyz/">hic et nunc</a> nft browser.
               This site is in very early alpha and is not ready to be used. But
               if you want to sneak a peek you can buy my lucky penny. Connect
               with a wallet that possesses my lucky penny and the site is yours.
@@ -37,8 +39,15 @@ class Login extends React.Component<any, any> {
               </a>
             </p>
             <p className="padding-top text-xs pt-4">
-              (If you are the first person to circumvent this I'll give you 10
-              tezos.)
+              (
+              <a
+                href="https://twitter.com/tezosmoonapp/status/1436211318340599810"
+                target="_blank"
+              >
+                If you are the first person to circumvent this I'll give you 10
+                tezos.
+              </a>
+              )
             </p>
           </div>
 
